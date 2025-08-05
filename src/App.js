6183000c76cf48dac74872f536e73f63ea413699
Anshul Bharat <96761799+anshul-ab17@users.noreply.github.com>
2025-08-05 17:06:13 +0000

    componet1

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const simulatedAPIResponse = {
     {
       text:
         "as the age of the deceased at the time of accident was held to be about 54–55 years by the learned Tribunal, being self-employed, as such, 10% of annual income should have been awarded on account of future prospects. (Para 7 of the document)",
-      source: "Dani_Devi_v_Pritam_Singh.pdf",
+      source: "Dan i_Devi_v_Pritam_Singh.pdf",
       link:
         "https://lexisingapore-my.sharepoint.com/:b:/g/personal/harshit_lexi_sg/EdOegeiR_gdBvQxdyW4xE6oBCDgj5E4Bo5wjvhPHpqgIuQ?e=TEu4vz",
     },
